7e9bcf80914fad03217be6fcff196b4a79dfcc9f
enkiwa <743668352@qq.com>
2021-03-08 21:10:36 +0800

    重新整理源文件

---
+++ cloudbackup/com/cilent/Java/JDBCTool.java
@@ -0,0 +1,32 @@
+package cilent.Java;
+
+import java.sql.*;
+
+public class JDBCTool {
+    private static String DRIVER =null;
+    private static String URL =null;
+    private static String USERNMAE =null;
+    private static String PASSWORD =null;
+
+    static{
+        try {
+            DRIVER = "com.mysql.cj.jdbc.Driver";
+            URL = "jdbc:mysql://8.140.101.185:3306/db_aliyun?userUnicode=true&characterEncoding=utf8&useSSL=true&serverTimezone=UTC";
+            USERNMAE = "root";
+            PASSWORD = "root";
+            Class.forName(DRIVER);
+        }  catch (Exception e) {
+            e.printStackTrace();
+        }
+    } //加载驱动
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL,USERNMAE,PASSWORD);
+    } //获取连接
+
+    public static void relase(Connection con, Statement st, ResultSet rs) throws SQLException{
+        if(rs!=null){rs.close();}
+        if(st!=null){st.close();}
+        if(con!=null){con.close();}
+    } //释放连接
+}
---
+++ cloudbackup/com/cilent/Java/Main.java
@@ -0,0 +1,9 @@
+package cilent.Java;
+
+public class Main {
+    public static void main(String[] args) {
+        Menu me = new Menu();
+        me.Hello();
+
+    }
+}
